08c81919771e6b5c7ff01b8ceaaf7cd70eef8d0c
bstriker <strikeforcezero@gmail.com>
2023-10-31 14:48:14 -0400

    feat(components): add piece type and color to ChessBoardPiece classnames

--- src/components/ChessBoardPiece/index.tsx
+++ src/components/ChessBoardPiece/index.tsx
@@ -1,16 +1,28 @@
-import { HTMLProps } from 'react';
-import { ChessPiece } from '../../engine/src/piece/ChessPiece';
+import React, { HTMLProps } from 'react';
+import {
+  ChessPiece,
+  isColoredPieceContainer,
+} from '../../engine/src/piece/ChessPiece';
 import { chessPieceToUnicode } from '../../engine/src/utils/print/unicode';
 
-export function ChessBoardPiece(
+function getPieceAndColor(piece: ChessPiece): string[] {
+  if (!isColoredPieceContainer(piece)) {
+    return [];
+  }
+  return [piece.coloredPiece.color, piece.coloredPiece.pieceType];
+}
+
+function ChessBoardPieceComponent(
   props: {
     piece: ChessPiece,
     spanProps?: HTMLProps<HTMLSpanElement>,
   },
 ) {
   return (
-    <span {...props.spanProps}>
+    <span {...props.spanProps} className={getPieceAndColor(props.piece).join(' ')}>
       {chessPieceToUnicode(props.piece)}
     </span>
   )
 }
+
+export const ChessBoardPiece = React.memo(ChessBoardPieceComponent);
